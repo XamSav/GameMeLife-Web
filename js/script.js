20ed60e908c9e6e57125814c5b8826e701378ef4
Jean0612 <jeanpol.velez@hotmail.com>
2025-05-16 00:41:49 +0200

    js Jv

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const openFormBtn = document.getElementById("openForm");
+const closeFormBtn = document.getElementById("closeForm");
+const taskForm = document.getElementById("taskForm");
+
+openFormBtn.addEventListener("click", openForm);
+closeFormBtn.addEventListener("click", closeForm);
+
+function openForm() {
+    taskForm.classList.remove("hidden");
+}
+
+function closeForm() {
+    
+    taskForm.classList.add("hidden");
+}
